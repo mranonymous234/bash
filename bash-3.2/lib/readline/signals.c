b22f3d638ca0c0ff02e2e5a7f3e208366dce1751
mranonymous234 <chathu_cna@icloud.com>
2026-05-09 21:42:37 +0530

    Update signals.c

--- bash-3.2/lib/readline/signals.c
+++ bash-3.2/lib/readline/signals.c
@@ -69,7 +69,11 @@ typedef RETSIGTYPE SigHandler ();
 typedef struct sigaction sighandler_cxt;
 #  define rl_sigaction(s, nh, oh)	sigaction(s, nh, oh)
 #else
-typedef struct { SigHandler *sa_handler; int sa_mask, sa_flags; } sighandler_cxt;
+typedef struct {
+    void (*handler)(int);
+    int sa_mask;
+    int sa_flags;
+} sighandler_cxt;
 #  define sigemptyset(m)
 #endif /* !HAVE_POSIX_SIGNALS */
 
